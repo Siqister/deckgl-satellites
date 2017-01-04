f85a9a37a84008a88237f6b470ea88e39fd6e753
siqister <siqi.zhu@gmail.com>
2017-01-03 22:05:18 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	entry:'./src/index.js',
+	output:{
+		path:__dirname+'/dist',
+		filename:'bundle.js',
+		publicPath:'/assets/'
+	},
+	module:{
+		loaders:[
+			{
+				test: /\.jsx?$/,
+				loader:'babel-loader',
+				query:{
+					presets:['es2015','react']
+				},
+				include:__dirname+'/src'
+			}
+		]
+	}
+}
