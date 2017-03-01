445bfac600679c65dc7d71a50621aa80bec09e0a
siqister <siqi.zhu@gmail.com>
2017-02-28 21:27:06 -0500

    Update dev server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import * as d3 from 'd3';
 
 console.log(d3);
+console.log('Starting index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 //run webpack-dev-server --content-base dist
+//https://webpack.github.io/docs/configuration.html
 
 module.exports = {
 	entry:'./src/index.js',
@@ -6,6 +7,10 @@ module.exports = {
 		path:__dirname+'/dist',
 		filename:'bundle.js',
 		publicPath:'/assets/'
+	},
+	devtool:"#source-map",
+	devServer:{
+		contentBase:'./dist'
 	},
 	module:{
 		loaders:[
@@ -16,7 +21,14 @@ module.exports = {
 					presets:['es2015','react']
 				},
 				include:__dirname+'/src'
-			}
+			},
+			{test:/\.css$/, loader:"style-loader!css-loader"},
+			{ test: /\.woff(\d+)?$/, loader: 'url?prefix=font/&limit=5000&mimetype=application/font-woff' },
+         	{ test: /\.ttf$/, loader: 'file?prefix=font/' },
+         	{ test: /\.eot$/, loader: 'file?prefix=font/' },
+         	{ test: /\.svg$/, loader: 'file?prefix=font/' },
+         	{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"},
+         	{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
 		]
 	}
 }
