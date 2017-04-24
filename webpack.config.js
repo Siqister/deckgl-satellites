c7e37d1c76d4e5d47316f7d0c6cf4f1c9f96162e
siqister <siqi.zhu@gmail.com>
2017-04-24 09:28:58 -0400

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
 	entry:'./src/index.js',
 	output:{
-		path:path.join(__dirname,'/dist'),
+		path:path.join(__dirname,'/dist/assets'),
 		filename:'bundle.js',
 		publicPath:'/assets/'
 	},
